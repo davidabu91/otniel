2c0d2f404df8a92edc206e8e8c8652ddd7f8104a
David Abou <davidabu91@gmail.com>
2022-04-30 21:42:04 +0300

    ...

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,12 +5,8 @@ import theme from 'theme';
 import SEO from 'components/seo';
 import Layout from 'components/layout';
 import Banner from '../sections/banner';
-import KeyFeature from '../sections/key-feature';
 import About from '../sections/about';
 import Business from '../sections/business';
-import CoreFeature from '../sections/security';
-import WorkFlow from '../sections/education';
-import Package from '../sections/package';
 import TeamSection from '../sections/team-section';
 import TestimonialCard from '../sections/testimonial';
 import Services from 'sections/services';
@@ -22,12 +18,11 @@ export default function IndexPage() {
     <ThemeProvider theme={theme}>
         <Layout>
           <SEO title="Otniel" />
-          <Banner />
+          <Banner/>
           <Services />
           <About />
           <Business />
           <Education />
-          {/* <Package /> */}
           <Security />
 
           <TeamSection />
